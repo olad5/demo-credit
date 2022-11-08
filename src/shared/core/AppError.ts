ef8a6f7d0721952042ed65bdfb7f8a4004064590
Francis Shonubi <shonubioladipo@gmail.com>
2022-11-08 10:16:09 +0100

    feat: add Unexpected Error class
    - this is used for times when there is an error and the server does not
      know how to handle it

---
+++ src/shared/core/AppError.ts
@@ -0,0 +1,20 @@
+import { Result } from "./Result";
+import { UseCaseError } from "./UseCaseError";
+import debug from "debug";
+
+const log = debug("app:AppError");
+
+export class UnexpectedError extends Result<UseCaseError> {
+  public constructor(err: any) {
+    super(false, {
+      message: `An unexpected error occurred.`,
+      error: err
+    } as UseCaseError);
+    log(`[AppError]: An unexpected error occurred`);
+    log(err);
+  }
+
+  public static create(err: any): UnexpectedError {
+    return new UnexpectedError(err);
+  }
+}
