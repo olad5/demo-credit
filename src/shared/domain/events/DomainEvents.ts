ca769c5c0e3efd48a21b60b92508c3165e6b6020
Francis Shonubi <shonubioladipo@gmail.com>
2022-11-09 07:53:00 +0100

    feat: add the Aggregate Root, DomainEvents classes
    - Base classes for Aggregates and domain events

---
+++ src/shared/domain/events/DomainEvents.ts
@@ -0,0 +1,99 @@
+import { IDomainEvent } from "./IDomainEvent";
+import { AggregateRoot } from "../AggregateRoot";
+import { UniqueEntityID } from "../UniqueEntityID";
+
+type HandlerFunction = (event: IDomainEvent) => Promise<void>;
+
+export class DomainEvents {
+  private static handlersMap:
+    | {
+        IDomainEvent: [HandlerFunction];
+      }
+    | Record<string, never> = {};
+
+  private static markedAggregates: AggregateRoot<any>[] = [];
+
+  public static markAggregateForDispatch(aggregate: AggregateRoot<any>): void {
+    const aggregateFound = this.findMarkedAggregateByID(aggregate.id);
+    const wasAggregateFound = !!aggregateFound;
+
+    if (!wasAggregateFound) {
+      this.markedAggregates.push(aggregate);
+      return;
+    }
+
+    if (aggregate.equals(aggregateFound)) {
+      this.removeAggregateFromMarkedDispatchList(aggregateFound);
+      this.markedAggregates.push(aggregate);
+    }
+  }
+
+  private static dispatchAggregateEvents(aggregate: AggregateRoot<any>): void {
+    aggregate.domainEvents.forEach((event: IDomainEvent) => {
+      this.dispatch(event);
+    });
+  }
+
+  private static removeAggregateFromMarkedDispatchList(
+    aggregate: AggregateRoot<any>
+  ): void {
+    const index = this.markedAggregates.findIndex((a) => a.equals(aggregate));
+    this.markedAggregates.splice(index, 1);
+  }
+
+  private static findMarkedAggregateByID(
+    id: UniqueEntityID
+  ): AggregateRoot<any> | null {
+    let found: AggregateRoot<any> | null = null;
+    for (const aggregate of this.markedAggregates) {
+      if (aggregate.id.equals(id)) {
+        found = aggregate;
+      }
+    }
+
+    return found;
+  }
+
+  public static dispatchEventsForAggregate(id: UniqueEntityID): void {
+    const aggregate = this.findMarkedAggregateByID(id);
+
+    if (aggregate) {
+      this.dispatchAggregateEvents(aggregate);
+      aggregate.clearEvents();
+      this.removeAggregateFromMarkedDispatchList(aggregate);
+    }
+  }
+
+  public static register(
+    callback: (event: IDomainEvent) => void,
+    eventClassName: string
+  ): void {
+    /* eslint-disable */
+    if (!this.handlersMap.hasOwnProperty(eventClassName)) {
+      this.handlersMap[eventClassName] = [];
+    }
+    /* eslint-enable */
+    this.handlersMap[eventClassName].push(callback);
+  }
+
+  public static clearHandlers(): void {
+    this.handlersMap = {};
+  }
+
+  public static clearMarkedAggregates(): void {
+    this.markedAggregates = [];
+  }
+
+  private static dispatch(event: IDomainEvent): void {
+    const eventClassName: string = event.constructor.name;
+
+    if (
+      Object.prototype.hasOwnProperty.call(this.handlersMap, eventClassName)
+    ) {
+      const handlers: HandlerFunction[] = this.handlersMap[eventClassName];
+      for (const handler of handlers) {
+        handler(event);
+      }
+    }
+  }
+}
