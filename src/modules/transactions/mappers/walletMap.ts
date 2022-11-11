bf3577fa3bc309035bbea1224d4d25c6582b50e5
Francis Shonubi <shonubioladipo@gmail.com>
2022-11-11 15:56:33 +0100

    feat: add wallet mapper

---
+++ src/modules/transactions/mappers/walletMap.ts
@@ -0,0 +1,39 @@
+import debug from "debug";
+import { UniqueEntityID } from "../../../shared/domain/UniqueEntityID";
+import { WalletModel } from "../../../shared/infra/database/knex/models/WalletModel";
+import { Mapper } from "../../../shared/infra/Mapper";
+import { UserId } from "../../users/domain/userId";
+import { Wallet } from "../domain/wallet";
+import { WalletBalance } from "../domain/walletBalance";
+
+export type WalletType = Pick<WalletModel, "id" | "balance" | "user_id">;
+
+const log = debug("app:WalletMap");
+
+export class WalletMap implements Mapper<Wallet> {
+  public static toDomain(knexModel: WalletType): Wallet | null {
+    const walletOrError = Wallet.create(
+      {
+        walletBalance: WalletBalance.create({
+          amount: knexModel.balance
+        }).getValue(),
+        userId: UserId.create(new UniqueEntityID(knexModel.user_id)).getValue()
+      },
+      new UniqueEntityID(knexModel.id)
+    );
+
+    walletOrError.isFailure ? log(walletOrError.getErrorValue()) : "";
+
+    return walletOrError.isSuccess ? walletOrError.getValue() : null;
+  }
+
+  public static async toPersistence(wallet: Wallet): Promise<WalletType> {
+    const baseWallet: WalletType = {
+      id: wallet.walletId.id.toString(),
+      balance: wallet.walletBalance.value,
+      user_id: wallet.userId.id.toString()
+    };
+
+    return baseWallet;
+  }
+}
